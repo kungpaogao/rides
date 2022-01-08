af6b1886883358210d9a719146bb281d8a4a0e94
Andrew <kungpaogao@gmail.com>
2022-01-07 23:42:28 -0500

    Initial website homepage and routes

---
+++ components/Layout.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import { ReactNode } from "react";
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Cornell Rides</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20">
+        {children}
+      </main>
+
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+        Built for fun :)
+      </footer>
+    </div>
+  );
+}
---
+++ pages/new-ride.tsx
@@ -0,0 +1,3 @@
+export default function NewRide() {
+  return <>new ride</>;
+}
