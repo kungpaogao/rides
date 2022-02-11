2e7bafc071c08ca5b4b5a3db10e83991cc3f4c16
Andrew <kungpaogao@gmail.com>
2022-02-11 02:47:46 -0500

    Route to login from nav bar

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import BasicButton from "./BasicButton";
 
 export default function Navigation({ className = "" }: { className?: string }) {
+  const { push } = useRouter();
+
   return (
     <div
       className={`flex w-full items-center border-b py-5 text-left ${className}`}
@@ -9,7 +12,7 @@ export default function Navigation({ className = "" }: { className?: string }) {
       <span className="flex-1 text-2xl font-bold">
         <Link href="/">Cornell Rides</Link>
       </span>
-      <BasicButton>Login</BasicButton>
+      <BasicButton onClick={() => push("/login")}>Login</BasicButton>
     </div>
   );
 }
