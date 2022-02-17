add141df67f067326380505850a3f5e2b54cda21
Andrew <kungpaogao@gmail.com>
2022-02-17 14:36:56 -0500

    Use tailwindcss-radix
    - replace use of Headless UI Transition
    - can eventually remove dependency on Headless UI
    - more consistent use of Radix

--- components/BasicDropdown.tsx
+++ components/BasicDropdown.tsx
@@ -1,6 +1,5 @@
-import { Transition } from "@headlessui/react";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import { Fragment, ReactNode, useState } from "react";
+import { ReactNode, useState } from "react";
 
 type BasicDropdownProps = {
   trigger?: ReactNode;
@@ -16,24 +15,15 @@ export default function BasicDropdown({
   return (
     <DropdownMenu.Root open={isMenuOpen} onOpenChange={setIsMenuOpen}>
       <DropdownMenu.Trigger asChild>{trigger}</DropdownMenu.Trigger>
-      <Transition
-        show={isMenuOpen}
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
+      <DropdownMenu.Content
+        sideOffset={5}
+        align="start"
+        className="w-96 rounded-md border bg-white p-1 shadow-md 
+        radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down 
+        md:w-48"
       >
-        <DropdownMenu.Content
-          sideOffset={5}
-          align="start"
-          className="w-96 rounded-md border bg-white p-1 shadow-md md:w-48"
-        >
-          {children}
-        </DropdownMenu.Content>
-      </Transition>
+        {children}
+      </DropdownMenu.Content>
     </DropdownMenu.Root>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        // Dropdown menu
+        "scale-in": {
+          "0%": { opacity: 0, transform: "scale(0)" },
+          "100%": { opacity: 1, transform: "scale(1)" },
+        },
+        "slide-down": {
+          "0%": { opacity: 0, transform: "translateY(-10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+        "slide-up": {
+          "0%": { opacity: 0, transform: "translateY(10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        // Dropdown menu
+        "scale-in": "scale-in 0.2s ease-in-out",
+        "slide-down": "slide-down 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
+        "slide-up": "slide-up 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
