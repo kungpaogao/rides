e8524d89829badd16395300e2ecc76a21aa5e868
Andrew <kungpaogao@gmail.com>
2022-03-05 23:00:21 -0500

    Implement page and endpoint for single ride detail
    - this will be page where users can see details and message driver
    - will probably want to move other rides endpoints to api/ride folder

---
+++ pages/api/ride/[id].ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../lib/prismaClient";
+import { queryToString } from "../../../lib/queryToString";
+import { checkAuth } from "../auth";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const {
+    query: { id },
+    method,
+  } = req;
+
+  try {
+    await checkAuth(req);
+
+    if (method === "GET") {
+      const ride = await prisma.ride.findUnique({
+        where: { id: queryToString(id) },
+      });
+      res.status(200).json(ride);
+    } else {
+      res.setHeader("Allow", ["GET"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  } catch (err: any) {
+    console.error(err);
+    res
+      .status(err.name === "Error" ? 500 : err.name)
+      .json({ error: { message: err.message } });
+  }
+};
--- pages/ride/[id].tsx
+++ pages/ride/[id].tsx
@@ -1,7 +1,68 @@
+import { Ride } from "@prisma/client";
 import { useRouter } from "next/router";
+import {
+  FiArrowLeft,
+  FiArrowRight,
+  FiCalendar,
+  FiDollarSign,
+  FiUser,
+} from "react-icons/fi";
+import BasicButton from "../../components/BasicButton";
+import BasicInput from "../../components/BasicInput";
+import BasicTextArea from "../../components/BasicTextArea";
+import { useFetchStatus } from "../../lib/useFetchStatus";
+import PageStatus from "../../types/PageStatus";
 
 export default function RideDetail() {
   const router = useRouter();
   const { id } = router.query;
-  return <>ride: {id}</>;
+
+  const {
+    data: ride,
+    error,
+    status: pageStatus,
+  } = useFetchStatus<Ride, Error>(router.isReady ? `/api/ride/${id}` : null);
+
+  if (pageStatus === PageStatus.Error) {
+    return <div className="prose w-full max-w-full py-7">{error?.message}</div>;
+  }
+
+  if (pageStatus === PageStatus.Success && ride) {
+    return (
+      <div className="prose py-7">
+        <BasicButton onClick={router.back} className="bg-white text-black">
+          <div className="flex items-center gap-2">
+            <FiArrowLeft /> Back
+          </div>
+        </BasicButton>
+
+        <h2 className="flex flex-wrap items-center">
+          {ride.fromAddr} <FiArrowRight className="mx-2" /> {ride.toAddr}
+        </h2>
+
+        <div className="flex items-center gap-3">
+          <FiCalendar /> {new Date(ride.datetime).toLocaleString()}
+        </div>
+        <div className="flex items-center gap-3">
+          <FiDollarSign />${ride.price} per seat
+        </div>
+        <div className="flex items-center gap-3">
+          <FiUser />
+          {ride.numSeats} seats (availability may differ)
+        </div>
+
+        <div className="mt-7 flex w-fit flex-col gap-3 rounded-lg border p-3 shadow-md">
+          <BasicInput label="Driver" disabled value={ride.email} />
+
+          <BasicTextArea
+            label="Message (optional)"
+            placeholder="Looking for a ride!"
+          />
+          <BasicButton>Request ride</BasicButton>
+        </div>
+      </div>
+    );
+  }
+
+  return <>Loading...</>;
 }
