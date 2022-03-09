da9aa0a82407ccfca70df0f496f844e93ccf6068
Andrew <kungpaogao@gmail.com>
2022-03-09 02:14:44 -0500

    Use default styles in BasicButton; add flat style
    - this allows the styles to be properly overridden using Tailwind
    - before, some default styles (bg-black, text-white) weren't changed
    - flat style removes all the shadows, so I can create text button

--- components/BasicButton.tsx
+++ components/BasicButton.tsx
@@ -4,17 +4,18 @@ type BasicButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   children?: ReactNode;
   className?: string;
   expand?: boolean;
+  flat?: boolean;
 };
 
 const BasicButton = forwardRef<HTMLButtonElement, BasicButtonProps>(
-  ({ children, className, expand, ...rest }, ref) => {
+  ({ children, className, expand = "", flat = "", ...rest }, ref) => {
     return (
       <button
         ref={ref}
-        className={`rounded border border-black bg-black px-2 py-1 
-        text-white shadow transition hover:shadow-md focus:shadow-md ${
-          expand && "w-full md:w-auto"
-        } ${className}`}
+        className={`transition d:rounded d:border d:border-black d:bg-black 
+        d:px-2 d:py-1 d:text-white ${
+          !flat && "d:shadow hover:d:shadow-md focus:d:shadow-md"
+        } ${expand && "w-full md:w-auto"} ${className}`}
         {...rest}
       >
         {children}
