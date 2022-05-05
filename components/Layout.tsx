8dc48e938b075599a68e9ed505cf0872b6605ec4
Andrew <kungpaogao@gmail.com>
2022-05-05 14:03:00 -0400

    Forward navigation props in layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import { ReactNode } from "react";
 import Footer from "./Footer";
-import Navigation from "./Navigation";
+import Navigation, { NavigationProps } from "./Navigation";
 
 type LayoutProps = {
   children: ReactNode;
+  navigationProps?: NavigationProps;
 };
 
-export default function Layout({ children }: LayoutProps) {
+export default function Layout({ children, navigationProps }: LayoutProps) {
   return (
     <div className="flex h-full min-h-screen flex-col">
       <Head>
@@ -16,7 +17,7 @@ export default function Layout({ children }: LayoutProps) {
       </Head>
 
       <nav className="w-full">
-        <Navigation className="px-5 lg:px-28" />
+        <Navigation {...navigationProps} />
       </nav>
 
       <main className="mb-12 w-full flex-1 px-5 lg:px-28">{children}</main>
