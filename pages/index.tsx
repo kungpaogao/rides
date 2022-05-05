8dea8699da87dfd5c19ba6a6dd9393a3a1ac7b62
Andrew <kungpaogao@gmail.com>
2022-05-05 13:59:11 -0400

    Use search bar component on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,13 @@
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/router";
-import { useRef, useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { FiSearch } from "react-icons/fi";
+import { useState } from "react";
 import BasicButton from "../components/BasicButton";
-import BasicInput from "../components/BasicInput";
 import BasicToast from "../components/BasicToast";
-import PlaceSearchInput from "../components/PlaceSearchInput";
-import { SearchRideQuery, SearchRideQuerySchema } from "../types/SearchRide";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   const { push } = useRouter();
-
-  const { register, handleSubmit, setValue } = useForm<SearchRideQuery>({
-    resolver: zodResolver(SearchRideQuerySchema),
-  });
-
-  const searchFromAutocompleteRef = useRef<HTMLInputElement | null>(null);
-  const searchToAutocompleteRef = useRef<HTMLInputElement | null>(null);
-  const { ref: searchFromRef, ...searchFromRest } = register("from", {
-    onBlur: (e) => {
-      setValue("from", e.target.value);
-    },
-  });
-  const { ref: searchToRef, ...searchToRest } = register("to", {
-    onBlur: (e) => {
-      setValue("to", e.target.value);
-    },
-  });
-
   // TODO: remove
   const [open, setOpen] = useState(true);
-
-  const onSubmit: SubmitHandler<SearchRideQuery> = async ({
-    from,
-    to,
-    datetime,
-  }) => {
-    const searchParams = new URLSearchParams({
-      from,
-      to,
-      dt: datetime.toISOString(),
-    });
-    push(`/search?${searchParams}`);
-  };
 
   return (
     <div className="w-full max-w-5xl py-7 lg:mx-auto">
@@ -55,61 +19,9 @@ export default function Home() {
         An open-source platform to post and search for rides.
       </h3>
 
-      <form
-        autoComplete="off"
-        onSubmit={handleSubmit(onSubmit)}
-        className="mx-auto mt-12 flex flex-wrap items-center gap-2 rounded-lg
-         border bg-gray-100 p-1 md:flex-nowrap"
-      >
-        <PlaceSearchInput
-          label="From"
-          placeholder="Ithaca, NY"
-          className="group w-full rounded-md border 
-          border-gray-100 p-3 focus-within:border-gray-200 
-          focus-within:bg-white focus-within:shadow-lg md:flex-1"
-          labelClassName="px-2"
-          inputClassName="w-full border-none bg-transparent"
-          autocompleteRef={searchFromAutocompleteRef}
-          ref={(e) => {
-            searchFromRef(e);
-            searchFromAutocompleteRef.current = e;
-          }}
-          {...searchFromRest}
-        />
-        <PlaceSearchInput
-          label="To"
-          placeholder="New York, NY"
-          className="group w-full rounded-md border 
-          border-gray-100 p-3 focus-within:border-gray-200 
-          focus-within:bg-white focus-within:shadow-lg md:flex-1"
-          labelClassName="px-2"
-          inputClassName="w-full border-none bg-transparent"
-          autocompleteRef={searchToAutocompleteRef}
-          ref={(e) => {
-            searchToRef(e);
-            searchToAutocompleteRef.current = e;
-          }}
-          {...searchToRest}
-        />
-        <BasicInput
-          label="Date"
-          type="date"
-          className="group w-full rounded-md border border-gray-100 
-          p-3 focus-within:border-gray-200 focus-within:bg-white 
-          focus-within:shadow-lg md:flex-1"
-          labelClassName="px-2"
-          inputClassName="w-full border-none bg-transparent"
-          {...register("datetime", { valueAsDate: true })}
-        />
-        <BasicButton
-          className="mx-3 mb-3 flex w-full items-center justify-center 
-          rounded-lg py-3 hover:shadow-lg md:mb-0 md:aspect-square md:w-12"
-          type="submit"
-        >
-          <FiSearch size={20} title="Search" />
-          <span className="ml-2 md:hidden">Search</span>
-        </BasicButton>
-      </form>
+      <div className="mt-12">
+        <SearchBar />
+      </div>
       <div className="my-7 flex w-full items-center gap-5">
         <span className="flex-1 border-b-2" />
         <p>or</p>
